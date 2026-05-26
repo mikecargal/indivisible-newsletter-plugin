20550ec4d5c6c5a0116bc4ca7cc0f533bc6dd849
Mike Cargal <mike@cargal.net>
2026-05-26 13:39:50 -0400

    Open v1.1.7-dev

--- src/indivisible-newsletter.php
+++ src/indivisible-newsletter.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Indivisible Newsletter Poster
  * Description: Automatically monitors an email inbox and creates WordPress posts from newsletter emails.
- * Version: 1.1.6
+ * Version: 1.1.7-dev
  * Author: Mike Cargal
  */
 
